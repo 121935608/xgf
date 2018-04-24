0ad5522bb08e5903b3947dc618b941d652f9222d
chenmengzhen <chenmengzhen@istarfinancial.cn>
2018-04-24 15:32:19 +0800

    谢鲜CRM后台文件夹

---
+++ src/main/java/com/xingrongjinfu/crm/department/controller/DeptController.java
@@ -0,0 +1,30 @@
+package com.xingrongjinfu.crm.department.controller;
+
+import org.framework.core.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.xingrongjinfu.crm.department.common.DeptConstant;
+import com.xingrongjinfu.system.SystemConstant;
+
+/**
+ * 部门管理  处理
+ * 
+ * @author y
+ */
+@Controller
+@RequestMapping(SystemConstant.CRM_URL)
+public class DeptController extends BaseController
+{
+
+    /**
+     * 跳转角色列表界面
+     */
+    @RequestMapping(DeptConstant.DEPT_URL)
+    public ModelAndView loadCrmDept()
+    {
+        return this.getModelAndView(DeptConstant.DEPT_PAGE);
+    }
+
+}
